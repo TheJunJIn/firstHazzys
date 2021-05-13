44228a61edad2c2266b612456d1fbc78757cf99c
Sujak <sujak@cttd.co.kr>
2021-05-13 15:02:49 +0900

    update demo

--- dist/asset/js/main.js
+++ dist/asset/js/main.js
@@ -1,14 +1,1 @@
-/******/ (function() { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************!*\
-  !*** ./src/asset/js/main.js ***!
-  \******************************/
-var _window = window,
-    $ = _window.$;
-console.log('main.js');
-$(function () {
-  console.log('jQuery');
-});
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9wcm9qZWN0LWhhenp5cy0yMDIxLy4vc3JjL2Fzc2V0L2pzL21haW4uanMiXSwibmFtZXMiOlsid2luZG93IiwiJCIsImNvbnNvbGUiLCJsb2ciXSwibWFwcGluZ3MiOiI7Ozs7O2NBQWNBLE07SUFBTkMsQyxXQUFBQSxDO0FBQ1JDLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLFNBQVo7QUFFQUYsQ0FBQyxDQUFDLFlBQVU7QUFDVkMsU0FBTyxDQUFDQyxHQUFSLENBQVksUUFBWjtBQUNELENBRkEsQ0FBRCxDIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCB7ICQgfSA9IHdpbmRvdztcbmNvbnNvbGUubG9nKCdtYWluLmpzJyk7XG5cbiQoZnVuY3Rpb24oKXtcbiAgY29uc29sZS5sb2coJ2pRdWVyeScpO1xufSlcbiJdLCJzb3VyY2VSb290IjoiIn0=
+!function(){var o=window.$;console.log("main.js"),o((function(){console.log("jQuery")}))}();
