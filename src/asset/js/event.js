41fb8f11f0bb145eb8a5f698de80fa42ce64af21
sujak <sujak@cttd.co.kr>
2021-05-28 15:03:16 +0900

    css,js 수정

--- src/asset/js/event.js
+++ src/asset/js/event.js
@@ -1,8 +1,4 @@
 import AOS from 'aos';
-import rangesliderJs from 'rangeslider-js'
-import CategoryNavigation from '../../components/navigation/list-category';
-
 window.AOS = AOS;
-window.rangesliderJs = rangesliderJs;
 
 AOS.init({ delay: 100, offset: 10 });
